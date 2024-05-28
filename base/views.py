ddee9a68e84b3118d9914a0992dbf74d16f72ac3
Vinicios <vinicioscsoares@gmail.com>
2024-05-28 15:22:58 -0300

    botão de deletar posto

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 # Create your views here.
 # rooms = [
 #     {'id': 1, 'name': 'vamos aprender python'},
@@ -19,3 +20,46 @@ def room(request,pk):
     context = {'room': room}
 
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+
+    form = RoomForm
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+
+    context = {'form': form }
+
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request,pk):
+
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method =='POST':
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect ('home')
+
+    context = {
+        'room':room,
+        'form': form,
+    }
+
+    return render(request,'base/room_form.html', context) 
+
+def deleteRoom(request, pk):
+
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    context = {'obj':room}
+    
+    return render(request, 'base/delete.html', context)
