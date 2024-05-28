8e5a383b1ce449708aa17f530ed25ccd709e11d5
Vinicios <vinicioscsoares@gmail.com>
2024-05-28 05:30:49 -0300

    Models Room, Topic e Message, criados

--- base/views.py
+++ base/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
 # Create your views here.
-rooms = [
-    {'id': 1, 'name': 'vamos aprender python'},
-    {'id': 2, 'name': 'Aprenda C++'},
-    {'id': 3, 'name': 'Aprenda JavaScript'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'vamos aprender python'},
+#     {'id': 2, 'name': 'Aprenda C++'},
+#     {'id': 3, 'name': 'Aprenda JavaScript'},
+# ]
 
 def home(request):
-    context = {'rooms':rooms}
+    rooms = Room.objects.all()
+    context = {
+        'rooms':rooms
+        }
     return render(request, 'base/home.html', context)
 
 def room(request,pk):
-    return render(request, 'base/room.html')
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+
+    return render(request, 'base/room.html', context)
