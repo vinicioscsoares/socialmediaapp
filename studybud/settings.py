a66c5a99b0b58bfe8174dc6e2ab79b2daa6997f5
Vinicios <vinicioscsoares@gmail.com>
2024-05-31 10:24:41 -0300

    static Files configurados

--- studybud/settings.py
+++ studybud/settings.py
@@ -125,6 +125,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static/'
 
 ]
+
+#STATIC_ROOT =
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
