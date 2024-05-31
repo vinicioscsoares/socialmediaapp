ee0af6e8f2ac81b6bc31592d6fdb4c7b2537a11e
Vinicios <vinicioscsoares@gmail.com>
2024-05-31 10:05:13 -0300

    componentes de Atividade Recente prontos

--- base/views.py
+++ base/views.py
@@ -112,6 +112,17 @@ def room(request,pk):
 
     return render(request, 'base/room.html', context)
 
+def userProfile(request,pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    rooms_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context= { 
+        'user':user,
+        'rooms':rooms,
+        'topics':topics,
+    }
+    return render(request, "base/profile.html",context)
 
 @login_required(login_url='login')
 def createRoom(request):
